672f16aec46e064d1b32f700c87c839bb4f3b59b
dstrivedi <drashti1118@gmail.com>
2020-09-07 14:57:17 +0530

    3 files modified

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+  console.log(key);
+  if (!audio) return;
+  audio.play();
+  key.classList.add("playing");
+  setInterval(() => {
+    key.classList.remove("playing");
+  }, 400);
+});
